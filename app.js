9c796a22f9a5f053b845287341d9a23ec5016c47
stacy_r <stacyrileyca@gmail.com>
2020-08-28 16:46:40 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//alert("Hi")
+document.addEventListener('DOMContentLoaded', () => {
+  //This is how to connect to the div w/class grid in html
+  const grid = document.querySelector('.grid')
+
+  //Array.from() collects all 200 divs and turns into an array
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+
+  const width = 10
+
+  console.log(squares)
+
+//34 minutes into video
+  //code goes here
+})
