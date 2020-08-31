73eed5a58e5820ab053459aabc68fc6484018707
stacy_r <stacyrileyca@gmail.com>
2020-08-31 17:13:07 -0400

    Created the tetromino arrays

--- app.js
+++ app.js
@@ -10,8 +10,78 @@ const StartBtn = document.querySelector('#start-button')
 
   const width = 10
 
-  console.log(squares)
+//The Tetrominoes
+const jrTetromino = [
+  [1, width+1, width*2+1, 2],
+  [width, width+1, width+2, width*2+2],
+  [1, width+1, width*2+1, width*2],
+  [width, width*2, width*2+1, width*2+2]
+]
 
-//34 minutes into video
+const jlTetromino = [
+  [0, 1, width+1, width*2+1],
+  [width, width+1, width+2, width*2],
+  [1, width+1, width*2+1, width*2+2],
+  [width*2, width*2+1, width*2+2, width+2]
+]
+
+const sTetromino = [
+  [width+1, width+2, width*2, width*2+1],
+  [0, width, width+1, width*2+1],
+  [width+1, width+2, width*2, width*2+1],
+  [0, width, width+1, width*2+1]
+]
+
+const zTetromino = [
+  [width, width+1, width*2+1, width*2+2],
+  [2, width+1, width+2, width*2+1],
+  [width, width+1, width*2+1, width*2+2],
+  [2, width+1, width+2, width*2+1]
+]
+
+const tTetromino = [
+  [1, width, width+1, width+2],
+  [1, width+1, width+2, width*2+1],
+  [width, width+1, width+2, width*2+1],
+  [1, width, width+1, width*2+1]
+]
+
+const bTetromino = [
+  [0, 1, width, width+1],
+  [0, 1, width, width+1],
+  [0, 1, width, width+1],
+  [0, 1, width, width+1]
+]
+
+const lTetromino = [
+  [1, width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3],
+  [1, width+1, width*2+1, width*3+1],
+  [width, width+1, width+2, width+3]
+]
+
+const theTetrominos = [jrTetromino, jlTetromino, sTetromino, zTetromino, tTetromino, bTetromino, lTetromino]
+
+let currentPosition = 4
+let currentRotation = 0
+
+//randomly select a Tetromino and its first rotation
+let random = Math.floor(Math.random()*theTetrominos.length)
+let current = theTetrominos[random][currentRotation]
+
+//draw the Tetromino
+function draw() {
+  current.forEach(index => {
+    squares[currentPosition + index].classList.add('tetromino')
+  })
+}
+
+//undraw the Tetromino
+function undraw() {}
+
+
+
+
+//44:21 minutes into video
   //code goes here
 })
