b44ee13340b5081fd57d595f3fdd4496c5a2c27a
stacy_r <stacyrileyca@gmail.com>
2020-09-01 17:06:00 -0400

    Working on code that draws tetromino shapes

--- app.js
+++ app.js
@@ -1,87 +1,104 @@
-//alert("Hi")
+  //  alert  ("Hi")
 document.addEventListener('DOMContentLoaded', () => {
   //This is how to connect to the div w/class grid in html
   const grid = document.querySelector('.grid')
 
   //Array.from() collects all 200 divs and turns into an array
   let squares = Array.from(document.querySelectorAll('.grid div'))
-const ScoreDisplay = document.querySelector('#score')
-const StartBtn = document.querySelector('#start-button')
-
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
+  //let index = 0
   const width = 10
 
-//The Tetrominoes
-const jrTetromino = [
-  [1, width+1, width*2+1, 2],
-  [width, width+1, width+2, width*2+2],
-  [1, width+1, width*2+1, width*2],
-  [width, width*2, width*2+1, width*2+2]
-]
+  //  The Tetrominoes
+  const jrTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
 
-const jlTetromino = [
-  [0, 1, width+1, width*2+1],
-  [width, width+1, width+2, width*2],
-  [1, width+1, width*2+1, width*2+2],
-  [width*2, width*2+1, width*2+2, width+2]
-]
+  const jlTetromino = [
+    [0, 1, width + 1, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2],
+    [1, width + 1, width * 2 + 1, width * 2 + 2],
+    [width * 2, width * 2 + 1, width * 2 + 2, width + 2]
+  ]
 
-const sTetromino = [
-  [width+1, width+2, width*2, width*2+1],
-  [0, width, width+1, width*2+1],
-  [width+1, width+2, width*2, width*2+1],
-  [0, width, width+1, width*2+1]
-]
+  const sTetromino = [
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1]
+  ]
 
-const zTetromino = [
-  [width, width+1, width*2+1, width*2+2],
-  [2, width+1, width+2, width*2+1],
-  [width, width+1, width*2+1, width*2+2],
-  [2, width+1, width+2, width*2+1]
-]
+  const zTetromino = [
+    [width, width + 1, width * 2 + 1, width * 2 + 2],
+    [2, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width * 2 + 1, width * 2 + 2],
+    [2, width + 1, width + 2, width * 2 + 1]
+  ]
 
-const tTetromino = [
-  [1, width, width+1, width+2],
-  [1, width+1, width+2, width*2+1],
-  [width, width+1, width+2, width*2+1],
-  [1, width, width+1, width*2+1]
-]
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+  ]
 
-const bTetromino = [
-  [0, 1, width, width+1],
-  [0, 1, width, width+1],
-  [0, 1, width, width+1],
-  [0, 1, width, width+1]
-]
+  const bTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+  ]
 
-const lTetromino = [
-  [1, width+1, width*2+1, width*3+1],
-  [width, width+1, width+2, width+3],
-  [1, width+1, width*2+1, width*3+1],
-  [width, width+1, width+2, width+3]
-]
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+  ]
 
-const theTetrominos = [jrTetromino, jlTetromino, sTetromino, zTetromino, tTetromino, bTetromino, lTetromino]
+  const theTetrominoes = [jrTetromino, jlTetromino, sTetromino, zTetromino, tTetromino, bTetromino, lTetromino]
 
-let currentPosition = 4
-let currentRotation = 0
+  let currentPosition = 4
+  let currentRotation = 0
 
-//randomly select a Tetromino and its first rotation
-let random = Math.floor(Math.random()*theTetrominos.length)
-let current = theTetrominos[random][currentRotation]
+  console.log(theTetrominoes[0][0])
 
-//draw the Tetromino
-function draw() {
-  current.forEach(index => {
-    squares[currentPosition + index].classList.add('tetromino')
-  })
-}
+  //randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation]
 
-//undraw the Tetromino
-function undraw() {}
+  //draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+      //squares[currentPosition + index].style.backgroundColor = colors[random]
+    })
+  } 
+
+  //undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+      //squares[currentPosition + index].style.backgroundColor = ''
+
+    })
+  }
+
+  //  make the Tetromino move down every second
+  timerId = setInterval(moveDown, 1000)
+
+  //  move down function
+  function moveDown () {
+    undraw()
+    currentPosition += width
+    draw()
+  }
 
 
-
-
-//44:21 minutes into video
-  //code goes here
+  //  44:21 minutes into video
+  //  code goes here
 })
